670ea9a9cb1f0f14ff2259464dbccfdd5b5d2074
Charles <funkcharles@hotmail.com>
2024-07-24 12:07:45 -0500

    Complete 3.13 & 3.14
    Add DB integration for frontend phonebook modification

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,37 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const app = express()
+
+//Middleware
 app.use(express.json())
+app.use(cors())
+app.use(morgan(':method :url :status :response-time ms - :body'))
+app.use(express.static('dist'))
+
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
 })
-app.use(morgan(':method :url :status :response-time ms - :body'))
-app.use(cors())
-app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+//Person Model
+let persons = []
+const Person = require('./models/person')
 
 //Root route
 app.get('/', (request, response) => {
@@ -44,17 +32,20 @@ app.get('/info', (request, response) => {
   request.requestTime = new Date();
   console.log(request.requestTime);
   response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
+    `<p>Phonebook has info for ${Person.length} people</p>
     </br>
     <p>${request.requestTime}</p>
     `)  
 })
 
-//Persons Routes
+//Get all persons
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json((persons))
+  })
 })
 
+//Get persons by ID
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
   const person = persons.find(p => p.id == id)
@@ -65,42 +56,32 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+//Delete person by ID
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(id).then(person => {
+    response.json(person)
+  })
 })
 
-
-const generateId = () => {
-  return Math.floor(Math.random() * 100)
-}
-
+//Add new person
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  
-  const personExists = persons.some(person => 
-    person.name.toLowerCase() === body.name.toLowerCase())
 
   if(!body.name || !body.number) {
     response.status(400).json({
       error: 'content missing'
     })
-  } else if (personExists) {
-    response.status(400).json({ 
-      error: 'name must be unique' 
-    })
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
   
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 //Used when endpoint of a requested route is unknown 
@@ -111,6 +92,7 @@ const unknownEndpoint = (request, response) => {
 //should be the last middleware 
 app.use(unknownEndpoint)
 
+//Start server
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
