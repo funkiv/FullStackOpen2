55708a385927b9584f64633d52223851369eb7be
Charles <funkcharles@hotmail.com>
2024-07-24 20:33:06 -0500

    Complete 3.15, 3.16, 3.17, 3.18 Phonebook DB
    Add delete for backend
    Consolidate error handling to one handler
    Add updating of phone number for a person
    Update individual id handling and info route

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -13,12 +13,21 @@ app.use(cors())
 app.use(morgan(':method :url :status :response-time ms - :body'))
 app.use(express.static('dist'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
 })
 
 //Person Model
-let persons = []
 const Person = require('./models/person')
 
 //Root route
@@ -31,11 +40,13 @@ app.get('/info', (request, response) => {
   console.log(new Date());
   request.requestTime = new Date();
   console.log(request.requestTime);
-  response.send(
-    `<p>Phonebook has info for ${Person.length} people</p>
-    </br>
-    <p>${request.requestTime}</p>
-    `)  
+  Person.countDocuments({}).then(count => {
+    response.send(
+      `<p>Phonebook has info for ${count} people</p>
+      </br>
+      <p>${request.requestTime}</p>
+      `)  
+  })
 })
 
 //Get all persons
@@ -47,13 +58,14 @@ app.get('/api/persons', (request, response) => {
 
 //Get persons by ID
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(p => p.id == id)
-  if (person) {
-    response.json(person)
-  } else {
-      response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
 })
 
 //Delete person by ID
@@ -84,6 +96,21 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.content,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 //Used when endpoint of a requested route is unknown 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint'})
@@ -91,6 +118,8 @@ const unknownEndpoint = (request, response) => {
 //handles all routes not handled by previous routes
 //should be the last middleware 
 app.use(unknownEndpoint)
+
+app.use(errorHandler)
 
 //Start server
 const PORT = process.env.PORT || 3001
