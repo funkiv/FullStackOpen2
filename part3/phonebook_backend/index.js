53fb2b6b8279d349b33538564e19742d1a00f378
Charles <funkcharles@hotmail.com>
2024-07-26 15:04:34 -0500

    Complete 3.19 Phonebook database
    Add validation for char length and required fields

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -18,8 +18,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
 
@@ -50,14 +51,15 @@ app.get('/info', (request, response) => {
 })
 
 //Get all persons
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json((persons))
   })
+  .catch(error => next(error))
 })
 
 //Get persons by ID
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -66,18 +68,20 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
+    .catch(error => next(error))
 })
 
 //Delete person by ID
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndDelete(id).then(person => {
     response.json(person)
   })
+  .catch(error => next(error))
 })
 
 //Add new person
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if(!body.name || !body.number) {
@@ -94,13 +98,14 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
   const person = {
-    name: body.content,
+    name: body.name,
     number: body.number,
   }
 
