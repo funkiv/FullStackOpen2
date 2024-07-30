0084830714cdb30781f7e84da129cdec51294fa0
Charles <funkcharles@hotmail.com>
2024-07-30 13:40:48 -0500

    Complete 4.5
    Add favoriteBlog function and test cases

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,8 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, currentValue) => acc + currentValue.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  
+  let topBlog = blogs.reduce((mostLiked, current) =>
+    mostLiked.likes < current.likes ? current : mostLiked,{ likes: 0, })
+
+  return({
+    title: topBlog.title,
+    author: topBlog.author,
+    likes: topBlog.likes
+  })
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
