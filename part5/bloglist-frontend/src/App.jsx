214e63d302ebd07b21872d0166d4f2e3a5dcbce3
Charles <funkcharles@hotmail.com>
2024-08-18 16:23:52 -0500

    Complete 5.2
    Add local storage usage for persisting login.
    Add log out button that clears local storage and user hook.

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -17,12 +17,24 @@ const App = () => {
     }  
   }, [user])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -30,6 +42,12 @@ const App = () => {
     } catch (exception) {
       console.log(exception)
     }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.clear()
+    setUser(null)
   }
 
   if (user === null) {
@@ -63,6 +81,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} is logged in</p>
+      <button onClick={handleLogout}>Log out</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
