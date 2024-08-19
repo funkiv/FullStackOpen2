f2ac4b522679e64b7b56a2231cfceeb775a63f8e
Charles <funkcharles@hotmail.com>
2024-08-18 19:08:09 -0500

    Complete 5.3
    Add blog creation form

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: ''
+  })
 
   useEffect(() => {
     if (user) {
@@ -50,6 +55,24 @@ const App = () => {
     setUser(null)
   }
 
+  const handleFormChange = (event) => {
+    const { name, value } = event.target
+    setNewBlog(prevBlog => ({
+      ...prevBlog,
+      [name]: value
+    }))}
+
+  const handleFormSubmit = async (event) => {
+      event.preventDefault()
+      const response = await blogService.create(newBlog)
+      console.log(response)
+      setNewBlog({
+        title: '',
+        author: '',
+        url: ''
+      })
+    }
+
   if (user === null) {
     return (
       <div>
@@ -82,6 +105,35 @@ const App = () => {
       <h2>blogs</h2>
       <p>{user.name} is logged in</p>
       <button onClick={handleLogout}>Log out</button>
+      <h1>create new</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          title:
+          <input
+          type="title"
+          value={newBlog.title}
+          name="title"
+          onChange={handleFormChange}/>
+        </div>
+        <div>
+          author:
+          <input
+          type="author"
+          value={newBlog.author}
+          name="author"
+          onChange={handleFormChange}/>
+        </div>
+        <div>
+          url:
+          <input
+          type="url"
+          value={newBlog.url}
+          name="url"
+          onChange={handleFormChange}/>
+        </div>
+        <button type='submit'>Create</button>
+      </form>
+      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
